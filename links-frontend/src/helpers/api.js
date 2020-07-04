e58a2d86e5497316af8cff7a3a1114854b08ba74
Anna Grazielle <grazielleconceicao@yahoo.com.br>
2020-07-03 22:55:43 -0300

    Primeiro commit

---
+++ links-frontend/src/helpers/api.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import { getToken } from './account';
+
+export const getApiUrl = (path) => {
+    return `http://localhost:3001${path}`;
+};
+
+export const getHeaders = () => {
+    const token = getToken();
+    if(!token) return {};
+
+    return{
+        Authorization: `Bearer ${token}`,
+    }
+};
+
+export const apiPost = (path, data = {}) => {
+    const url = getApiUrl(path);
+    const options = {
+        headers: getHeaders(),
+    }
+
+    return axios.post(url, data, options);
+};
